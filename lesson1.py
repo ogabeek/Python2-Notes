15cfc66f6b3ae3a9a62aa63bf5bb3575a45dbbf0
ogabek <ogabek.masharipov@harbour.space>
2025-11-11 17:38:24 +0100

    INIT: first commit

---
+++ lesson1.py
@@ -0,0 +1,74 @@
+
+
+
+# print("hello", "meme", "heheh", sep = "\n", end ="!")
+# print("check")
+
+# print(f"hello world  {x:.2f} ")
+# print("\n\n")
+
+# x = 12
+
+# if x > 10  x < 10 :  #and or | & xor not in is   
+#     print("not equal ")
+# elif x == 10:
+#     print("equal")
+# else:
+#     print("invalid input")
+
+    
+    
+
+# print("the end")
+
+
+
+
+
+
+#datatypes
+# str = "hello", "5", "👀"
+# int = 5,6,7
+# float = 4.6 , 4.0
+# bool = True False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# loops 
+
+# _ = 5
+# __ = 10
+
+# __main__ = 15
+
+# for i in range(2,10,2):
+#     if i > 10:
+#         break
+#         continue
+#     print(i)
+
+
+# for i in range(1, 6, 1):
+#     print(" " * (6 - i) , "*" * i)
+
+num = 1
+for row in range(1, 10, 1):
+    for col in range(row):
+        print(num,  end = " ")
+        num+=1
+    print()
+
+    
+    
