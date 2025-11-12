05e8f5a8985ce6a3feb347d0b23e40385b669630
ogabek <ogabek.masharipov@harbour.space>
2025-11-12 17:20:37 +0100

    class started

---
+++ l3.py
@@ -0,0 +1,28 @@
+# import time
+
+# box = 20
+# delay = 0.05
+
+# for i in range(1000):
+#     for j in range(20):
+#         print(" " * (j % box), ".", " " * (box - j % box))
+#         time.sleep(delay)
+#     for j in range(20):
+#         print(" " * (box - j % box) , ".", " " * (j % box))
+#         time.sleep(delay)
+
+
+
+# import time
+
+
+# a = [0]*10
+# for i in range(10): 
+#     a[i] = [1]
+#     time.sleep(1)
+#     print("\033[2J\033[H", end="")
+
+#     print(*a)
+
+    
+
---
+++ lesson2.py
@@ -0,0 +1,57 @@
+
+
+
+
+
+# def count_el(a, target):
+#     total = 0
+#     for i in a:
+#         if i == target: 
+#             total+=1
+#     return total
+
+
+
+
+
+
+a = input().split()
+for i in range(len(a)):
+    a[i] = int(a[i])
+
+# print(a[0]+10)
+
+# print(count_el)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+# list
+
+
+# a = [10, "🥲", 4, 6, 7]
+
+# a.append(8)
+
+# a[0] = 2
+
+# x = a.pop()
+
+# # del a[-1]
+# print(x)
+
+# print(a)
+
+
+
+
