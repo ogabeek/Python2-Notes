528a8f3531067687973caa8766e59bd12c903380
ogabek <ogabek.masharipov@harbour.space>
2025-11-14 19:14:08 +0100

    frequency_analisys_example

---
+++ f_q.py
@@ -0,0 +1,12 @@
+import library
+import features
+import database
+
+def main()
+	...
+	
+
+
+	
+if __name__ == "__main__":
+	main()
---
+++ features.py
@@ -0,0 +1,10 @@
+def read_file():
+	...
+	return text
+	
+def frequency_dispertion():
+	...
+ 
+ 
+ def test_main()
+	...
