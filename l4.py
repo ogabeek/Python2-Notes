de71a139931ee0aae7dffffb327efb365bef3d75
ogabek <ogabek.masharipov@harbour.space>
2025-11-14 10:50:45 +0100

    lesson4

---
+++ l4.py
@@ -0,0 +1,88 @@
+
+
+# list array
+
+
+# arr = ["Ogabek",178.5, 3, False]
+
+# arr[0] = "Vladimir"
+
+# t = ("Ogabek",178.5, 3, False)
+
+# t = list(t)
+
+# t[0] = "Vladimir"
+
+# t = tuple(t)
+
+
+# print(type(arr))
+# print(type(t))
+
+# print(arr)
+# print(t)
+
+
+# a = 5
+# b = 10
+
+# a = b
+# b = a
+
+
+
+# t = a
+# a = b
+# b = t
+
+# a,b = b, a
+# a,b,c = intput(), 
+# print(a,b)
+
+
+
+
+
+
+
+# print()
+
+# arr = ["Ogabek",178.5, 3, False]
+# t = ("Ogabek",178.5, 3, False)
+# s = {"Ogabek",178.5, 3, [2,1] ,[2,1]}
+
+# print(s)
+
+
+
+
+
+# a,b,*c =  [3,4,5,7,6,7,8,9]
+
+# print(a)
+# print(b)
+# print(c)
+# print(type(c))
+
+# a = list(range(100,0,-10))
+
+# print(a)
+
+
+# a = (5,6,7,8)
+# b = (7,)
+
+
+
+# print(type(a))
+# print(type(b))
+
+# a = []
+# b = tuple()
+# c = {}
+
+
+
+
+
+
